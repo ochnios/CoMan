89b06e2091911d1c2892be9b456c3f0935749a68
Szymon <98458655+ochnios@users.noreply.github.com>
2023-03-11 21:07:20 +0100

    First steps with EF Core.

--- CoMan/Data/ApplicationDbContext.cs
+++ CoMan/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CoMan.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoMan.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<TopicModel> Topics { get; set; } = null!;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- CoMan/Models/TopicModel.cs
+++ CoMan/Models/TopicModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoMan.Models
 {
@@ -14,15 +15,24 @@ namespace CoMan.Models
     {
         [Key]
         public int TopicId { get; set; }
+
+        [Required]
         [DisplayName("Date added")]
-        public DateTime AddedDate { get; set; }
+        public DateTime? AddedDate { get; set; }
+
+        [Required]
         [DisplayName("Title")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
+
         [DisplayName("Description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
+
+        [Required]
         [DisplayName("Student limit")]
-        public int StudentLimit { get; set; }
+        public int? StudentLimit { get; set; }
+
+        [Required]
         [DisplayName("Status")]
-        public TopicStatus Status { get; set; }
+        public TopicStatus? Status { get; set; }
     }
 }
